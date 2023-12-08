abebdb0fc113757cce8e21f2c4c2ea902d27961e
SOFe <sofe2038@gmail.com>
2023-12-09 00:12:18 +0800

    d8p2 (bad)

--- src/day8.rs
+++ src/day8.rs
@@ -1,19 +1,28 @@
-#[derive(Clone, Copy, Default, PartialEq, Eq)]
+use bitvec::vec::BitVec;
+
+#[derive(Clone, Copy, Default, PartialEq, Eq, PartialOrd, Ord)]
 struct NodeId(u16);
 
 impl NodeId {
+    const REQUIRED_SPACE: usize = 26 * 26 * 26;
+
     const AAA: Self = Self::new([b'A', b'A', b'A']);
+    const AAZ: Self = Self::new([b'A', b'A', b'Z']);
     const ZZZ: Self = Self::new([b'Z', b'Z', b'Z']);
 
     const fn new(arr: [u8; 3]) -> Self {
         let d0 = (arr[0] - b'A') as u16;
         let d1 = (arr[1] - b'A') as u16;
         let d2 = (arr[2] - b'A') as u16;
-        Self(d0 * 26 * 26 + d1 * 26 + d2)
+        Self(d2 * 26 * 26 + d1 * 26 + d0)
     }
 
     fn usize(self) -> usize {
         self.0 as usize
+    }
+
+    fn from_usize(i: usize) -> Self {
+        Self(i as u16)
     }
 }
 
@@ -46,46 +55,78 @@ enum Dir {
     Right,
 }
 
-#[aoc_runner_derive::aoc(day8, part1)]
-pub fn part1(input: &str) -> u64 {
-    let mut lines = input.lines();
-    let steps = lines.next().unwrap();
-    let stepper = steps
-        .chars()
+fn parse_steps(line: &str) -> impl Iterator<Item = Dir> + Clone + '_ {
+    line.chars()
         .map(|c| match c {
             'L' => Dir::Left,
             'R' => Dir::Right,
-            _ => unreachable!(),
+            _ => unsafe{std::hint::unreachable_unchecked()},
         })
-        .cycle();
+        .cycle()
+}
 
-    let mut nodes = vec![Node::default(); 26 * 26 * 26];
+fn parse_graph<'t>(
+    lines: impl Iterator<Item = &'t str>,
+    mut mark: impl FnMut(NodeId),
+) -> Graph {
+    let mut nodes = [Node::default(); NodeId::REQUIRED_SPACE];
     for line in lines {
         if !line.is_empty() {
             let (src, node) = Node::parse(line);
             nodes[src.usize()] = node;
+            mark(src);
         }
     }
 
-    let mut loc = NodeId::AAA;
-    for (steps, dir) in (0..).zip(stepper) {
-        if loc == NodeId::ZZZ {
-            return steps;
-        }
-        loc = nodes[loc.usize()].go(dir);
-    }
-
-    unreachable!()
+    Graph{nodes}
 }
 
-#[aoc_runner_derive::aoc(day8, part2)]
+struct Graph {
+    nodes: [Node; NodeId::REQUIRED_SPACE],
+}
+
+impl Graph {
+    fn count(&self, initial: NodeId, stepper: impl Iterator<Item = Dir>, terminate: impl Fn(NodeId) -> bool) -> usize {
+        let mut state = initial;
+        for (steps, dir) in stepper.enumerate() {
+            if terminate(state) {
+                return steps;
+            }
+            state = self.nodes[state.usize()].go(dir);
+        }
+
+        unreachable!()
+    }
+}
+
+#[aoc_runner_derive::aoc(day8, part1)]
+pub fn part1(input: &str) -> u32 {
+    let mut lines = input.lines();
+    let stepper = parse_steps(lines.next().unwrap());
+    let graph = parse_graph(lines, |_| {});
+
+    graph.count(NodeId::AAA, stepper, |node| node == NodeId::ZZZ) as u32
+}
+
+#[aoc_runner_derive::aoc(day8, part2, EmpiricalProd)]
 pub fn part2(input: &str) -> u64 {
-    0
+    let mut lines = input.lines();
+
+    let mut present: BitVec = BitVec::repeat(false, NodeId::REQUIRED_SPACE);
+
+    let steps_line = lines.next().unwrap();
+    let stepper = parse_steps(steps_line);
+    let graph = parse_graph(lines, |node| present.set(node.usize(), true));
+
+    present[..676].iter_ones().map(|one| {
+        let count = graph.count(NodeId::from_usize(one), stepper.clone(), |node| NodeId::AAZ <= node && node <= NodeId::ZZZ);
+        (count / steps_line.len()) as u64
+    }).product::<u64>() * steps_line.len() as u64
 }
 
 #[cfg(test)]
 mod tests {
-    const SAMPLE: &str = r"LLR
+    const SAMPLE1: &str = r"LLR
 
 AAA = (BBB, BBB)
 BBB = (AAA, ZZZ)
@@ -93,11 +134,6 @@ ZZZ = (ZZZ, ZZZ)";
 
     #[test]
     fn test_part1() {
-        assert_eq!(super::part1(SAMPLE), 6);
-    }
-
-    #[test]
-    fn test_part2() {
-        assert_eq!(super::part2(SAMPLE), 71503);
+        assert_eq!(super::part1(SAMPLE1), 6);
     }
 }
