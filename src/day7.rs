a751a8a8ec953e3991b38a9455763fff4b8341c6
SOFe <sofe2038@gmail.com>
2023-12-09 04:12:11 +0000

    d7p2

--- src/day7.rs
+++ src/day7.rs
@@ -3,43 +3,108 @@ use arrayvec::ArrayVec;
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 struct Index(u8);
 
-fn index(b: char) -> Index {
+fn indexer_1(b: char) -> Index {
     Index(12 - "AKQJT98765432".find(b).unwrap() as u8)
 }
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
-struct Type(u8);
+fn indexer_2(b: char) -> Index {
+    Index(12 - "AKQT98765432J".find(b).unwrap() as u8)
+}
 
-fn get_type(hand: [Index; 5]) -> Type {
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+enum Type {
+    High,
+    OnePair,
+    TwoPairs,
+    ThreeOfKind,
+    FullHouse,
+    FourOfKind,
+    FiveOfKind,
+}
+
+fn get_type_1(hand: [Index; 5]) -> Type {
     let mut stats = [0u8; 13];
     for card in hand {
         stats[card.0 as usize] += 1;
     }
 
+    base_get_type(stats)
+}
+
+fn base_get_type(stats: [u8; 13]) -> Type {
     for (i, stat) in stats.into_iter().enumerate() {
-        return Type(match stat {
-            5 => 10,
-            4 => 9,
+        return match stat {
+            5 => Type::FiveOfKind,
+            4 => Type::FourOfKind,
             3 => {
                 if stats[i + 1..].contains(&2) {
-                    8
+                    Type::FullHouse
                 } else {
-                    7
+                    Type::ThreeOfKind
                 }
             }
             2 => {
                 if stats[i + 1..].contains(&3) {
-                    8
+                    Type::FullHouse
                 } else if stats[i + 1..].contains(&2) {
-                    6
+                    Type::TwoPairs
                 } else {
-                    5
+                    Type::OnePair
                 }
             }
             _ => continue,
-        });
+        };
     }
-    Type(4)
+    Type::High
+}
+
+fn get_type_2(hand: [Index; 5]) -> Type {
+    let mut stats = [0u8; 13];
+    for card in hand {
+        stats[card.0 as usize] += 1;
+    }
+
+    let joker = stats[0];
+    let normal = &stats[1..];
+
+    match joker {
+        4 | 5 => Type::FiveOfKind,
+        3 => if normal.contains(&2) {
+            Type::FiveOfKind
+        } else {
+            Type::FourOfKind
+        },
+        2 => {
+            for &stat in normal {
+                match stat {
+                    3 => return Type::FiveOfKind, // 2 + 3
+                    2 => return Type::FourOfKind, // 2 + 2, 1
+                    _ => {} // might have a 2 afterwards
+                }
+            }
+            // 2 + 1, 1, 1 (still better than TwoPairs 1 + 1, 1 + 1, 1)
+            Type::ThreeOfKind
+        }
+        1 => {
+            let mut stats2 = [0u8; 5];
+            for &stat in normal {
+                stats2[stat as usize] += 1;
+            }
+            if stats2[4] == 1 {
+                Type::FiveOfKind // 4 + 1
+            } else if stats2[3] == 1 {
+                Type::FourOfKind // 3 + 1, 1
+            } else if stats2[2] == 2 {
+                Type::FullHouse // 2 + 1, 2
+            } else if stats2[2] == 1 {
+                Type::ThreeOfKind // 2 + 1, 1, 1
+            } else {
+                Type::OnePair // 1 + 1, 1, 1, 1
+            }
+        }
+        0 => base_get_type(stats),
+        _ => unreachable!(),
+    }
 }
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
@@ -49,13 +114,16 @@ struct Line {
     bid: u16,
 }
 
-#[aoc_runner_derive::aoc(day7, part1)]
-pub fn part1(input: &str) -> u32 {
+fn solve(
+    input: &str,
+    indexer: impl Fn(char) -> Index + Copy,
+    get_type: impl Fn([Index; 5]) -> Type,
+) -> u32 {
     let mut lines: Vec<Line> = input
         .lines()
         .map(|line| {
             let mut line = line.chars();
-            let hand: ArrayVec<Index, 5> = line.by_ref().take(5).map(index).collect();
+            let hand: ArrayVec<Index, 5> = line.by_ref().take(5).map(indexer).collect();
             let hand = hand.into_inner().unwrap();
             let ty = get_type(hand);
             let bid = line
@@ -85,9 +153,14 @@ pub fn part1(input: &str) -> u32 {
     */
 }
 
+#[aoc_runner_derive::aoc(day7, part1)]
+pub fn part1(input: &str) -> u32 {
+    solve(input, indexer_1, get_type_1)
+}
+
 #[aoc_runner_derive::aoc(day7, part2)]
-pub fn part2(input: &str) -> u64 {
-    0
+pub fn part2(input: &str) -> u32 {
+    solve(input, indexer_2, get_type_2)
 }
 
 #[cfg(test)]
@@ -105,6 +178,6 @@ QQQJA 483";
 
     #[test]
     fn test_part2() {
-        assert_eq!(super::part2(SAMPLE), 71503);
+        assert_eq!(super::part2(SAMPLE), 5905);
     }
 }
