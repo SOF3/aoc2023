1153cb5101ba532325e7a5e265c7d5ccd818e2b8
SOFe <sofe2038@gmail.com>
2023-12-09 12:36:29 +0800

    provide packed and bitshift solutions for day8 for comparison

--- src/day8.rs
+++ src/day8.rs
@@ -1,47 +1,63 @@
 use bitvec::vec::BitVec;
 
+trait NodeId: Clone + Copy + Default + Eq + Ord {
+    fn new(arr: [u8; 3]) -> Self;
+    fn u16(self) -> u16;
+    fn from_u16(i: u16) -> Self;
+    fn usize(self) -> usize;
+    fn from_usize(i: usize) -> Self;
+}
+
 #[derive(Clone, Copy, Default, PartialEq, Eq, PartialOrd, Ord)]
-struct NodeId(u16);
+struct Packed(u16);
 
-impl NodeId {
-    const REQUIRED_SPACE: usize = 26 * 26 * 26;
-
-    const AAA: Self = Self::new([b'A', b'A', b'A']);
-    const AAZ: Self = Self::new([b'A', b'A', b'Z']);
-    const ZZZ: Self = Self::new([b'Z', b'Z', b'Z']);
-
-    const fn new(arr: [u8; 3]) -> Self {
+impl NodeId for Packed {
+    fn new(arr: [u8; 3]) -> Self {
         let d0 = (arr[0] - b'A') as u16;
         let d1 = (arr[1] - b'A') as u16;
         let d2 = (arr[2] - b'A') as u16;
         Self(d2 * 26 * 26 + d1 * 26 + d0)
     }
 
-    fn usize(self) -> usize {
-        self.0 as usize
+    fn u16(self) -> u16 { self.0 }
+    fn from_u16(i: u16) -> Self { Self(i) }
+    fn usize(self) -> usize { self.0 as usize }
+    fn from_usize(i: usize) -> Self { Self(i as u16) }
+}
+
+#[derive(Clone, Copy, Default, PartialEq, Eq, PartialOrd, Ord)]
+struct BitShift(u16);
+
+impl NodeId for BitShift {
+    fn new(arr: [u8; 3]) -> Self {
+        let d0 = (arr[0] - b'A') as u16;
+        let d1 = (arr[1] - b'A') as u16;
+        let d2 = (arr[2] - b'A') as u16;
+        Self(d2 << 10 | d1 << 5 | d0)
     }
 
-    fn from_usize(i: usize) -> Self {
-        Self(i as u16)
-    }
+    fn u16(self) -> u16 { self.0 }
+    fn from_u16(i: u16) -> Self { Self(i) }
+    fn usize(self) -> usize { self.0 as usize }
+    fn from_usize(i: usize) -> Self { Self(i as u16) }
 }
 
 #[derive(Clone, Copy, Default)]
-struct Node {
-    left: NodeId,
-    right: NodeId,
+struct Node<NodeIdT: NodeId> {
+    left: NodeIdT,
+    right: NodeIdT,
 }
 
-impl Node {
-    fn parse(line: &str) -> (NodeId, Node) {
+impl<NodeIdT: NodeId> Node<NodeIdT> {
+    fn parse(line: &str) -> (NodeIdT, Node<NodeIdT>) {
         let line = line.as_bytes();
-        let src = NodeId::new(line[0..3].try_into().unwrap());
-        let left = NodeId::new(line[7..10].try_into().unwrap());
-        let right = NodeId::new(line[12..15].try_into().unwrap());
+        let src = NodeIdT::new(line[0..3].try_into().unwrap());
+        let left = NodeIdT::new(line[7..10].try_into().unwrap());
+        let right = NodeIdT::new(line[12..15].try_into().unwrap());
         (src, Node { left, right })
     }
 
-    fn go(self, dir: Dir) -> NodeId {
+    fn go(self, dir: Dir) -> NodeIdT {
         match dir {
             Dir::Left => self.left,
             Dir::Right => self.right,
@@ -65,14 +81,14 @@ fn parse_steps(line: &str) -> impl Iterator<Item = Dir> + Clone + '_ {
         .cycle()
 }
 
-fn parse_graph<'t>(
+fn parse_graph<'t, NodeIdT: NodeId>(
     lines: impl Iterator<Item = &'t str>,
-    mut mark: impl FnMut(NodeId),
-) -> Graph {
-    let mut nodes = [Node::default(); NodeId::REQUIRED_SPACE];
+    mut mark: impl FnMut(NodeIdT),
+) -> Graph<NodeIdT> {
+    let mut nodes = [Node::default(); u16::MAX as usize];
     for line in lines {
         if !line.is_empty() {
-            let (src, node) = Node::parse(line);
+            let (src, node) = Node::<NodeIdT>::parse(line);
             nodes[src.usize()] = node;
             mark(src);
         }
@@ -81,12 +97,12 @@ fn parse_graph<'t>(
     Graph{nodes}
 }
 
-struct Graph {
-    nodes: [Node; NodeId::REQUIRED_SPACE],
+struct Graph<NodeIdT: NodeId> {
+    nodes: [Node<NodeIdT>; u16::MAX as usize],
 }
 
-impl Graph {
-    fn count(&self, initial: NodeId, stepper: impl Iterator<Item = Dir>, terminate: impl Fn(NodeId) -> bool) -> u32 {
+impl<NodeIdT: NodeId> Graph<NodeIdT> {
+    fn count(&self, initial: NodeIdT, stepper: impl Iterator<Item = Dir>, terminate: impl Fn(NodeIdT) -> bool) -> u32 {
         let mut state = initial;
         for (steps, dir) in (0..).zip(stepper) {
             if terminate(state) {
@@ -99,33 +115,47 @@ impl Graph {
     }
 }
 
-#[aoc_runner_derive::aoc(day8, part1)]
-pub fn part1(input: &str) -> u32 {
+fn aaa<NodeIdT: NodeId>() -> NodeIdT { NodeIdT::new(*b"AAA") }
+fn aaz<NodeIdT: NodeId>() -> NodeIdT { NodeIdT::new(*b"AAZ") }
+fn zzz<NodeIdT: NodeId>() -> NodeIdT { NodeIdT::new(*b"ZZZ") }
+
+#[aoc_runner_derive::aoc(day8, part1, Packed)]
+pub fn part1_packed(input: &str) -> u32 { part1::<Packed>(input) }
+#[aoc_runner_derive::aoc(day8, part1, BitShift)]
+pub fn part1_bitshift(input: &str) -> u32 { part1::<BitShift>(input) }
+
+fn part1<NodeIdT: NodeId>(input: &str) -> u32 {
     let mut lines = input.lines();
     let stepper = parse_steps(lines.next().unwrap());
-    let graph = parse_graph(lines, |_| {});
+    let graph: Graph<NodeIdT> = parse_graph(lines, |_| {});
 
-    graph.count(NodeId::AAA, stepper, |node| node == NodeId::ZZZ)
+    graph.count(aaa(), stepper, |node| node == zzz())
 }
 
-#[aoc_runner_derive::aoc(day8, part2, EmpiricalProd)]
-pub fn part2(input: &str) -> u64 {
+#[aoc_runner_derive::aoc(day8, part2, EmpiricalProd_Packed)]
+pub fn part2_packed(input: &str) -> u64 { part2::<Packed>(input) }
+#[aoc_runner_derive::aoc(day8, part2, EmpiricalProd_BitShift)]
+pub fn part2_bitshift(input: &str) -> u64 { part2::<BitShift>(input) }
+
+fn part2<NodeIdT: NodeId>(input: &str) -> u64 {
     let mut lines = input.lines();
 
-    let mut present: BitVec = BitVec::repeat(false, NodeId::REQUIRED_SPACE);
+    let mut present: BitVec = BitVec::repeat(false, u16::MAX as usize);
 
     let steps_line = lines.next().unwrap();
     let stepper = parse_steps(steps_line);
-    let graph = parse_graph(lines, |node| present.set(node.usize(), true));
+    let graph: Graph<NodeIdT> = parse_graph(lines, |node: NodeIdT| present.set(node.usize(), true));
 
     present[..676].iter_ones().map(|one| {
-        let count = graph.count(NodeId::from_usize(one), stepper.clone(), |node| NodeId::AAZ <= node && node <= NodeId::ZZZ);
+        let count = graph.count(NodeIdT::from_usize(one), stepper.clone(), |node| node >= aaz() && node <= zzz());
         (count as u64 / steps_line.len() as u64) as u64
     }).product::<u64>() * steps_line.len() as u64
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::day8::{BitShift, Packed};
+
     const SAMPLE1: &str = r"LLR
 
 AAA = (BBB, BBB)
@@ -133,7 +163,11 @@ BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)";
 
     #[test]
-    fn test_part1() {
-        assert_eq!(super::part1(SAMPLE1), 6);
+    fn test_part1_packed() {
+        assert_eq!(super::part1::<Packed>(SAMPLE1), 6);
+    }
+    #[test]
+    fn test_part1_bitshift() {
+        assert_eq!(super::part1::<BitShift>(SAMPLE1), 6);
     }
 }
